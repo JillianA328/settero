a7457cccb64ab3b0319d55e39b7f12037a72b42d
Jillian Amelung <jpamelung@gmail.com>
2021-05-31 19:51:37 -0400

    finished javasript for 5day

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ var temp = $("#tempEl");
 var wind = $("#windEl");
 var humidity = $("#humidEl");
 var uvIn = $("#uvEl");
-var apiKey = "27e890ac1ac349b8058e8451ef106db3";
+var weatherURL = "94f83bcd13e457e18b174c45bbe04458";
 var queryUrl = "";
+var foreCastURL = "9357eff8b54eb1aa593011e2d69b6287";
+
+
 
 $("#searchBtn").on("click", function () {
   //get the value from the input
   city = $("#searchCity").val();
 
-  queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherURL}`;
 
   //clear value from the input
   $("#searchCity").val("");
@@ -44,11 +47,13 @@ function getCurrentWeather() {
       console.log(lon, lat);
 
       getForecast(lat, lon);
-    });
+      getFiveDayForecast(lat, lon);
+        });
+      }
 
   function getForecast(lat, lon) {
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherURL}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -78,13 +83,12 @@ function getCurrentWeather() {
         uvIndex(data);
       });
   }
-}
+
 
 // change the uv index color
 function uvIndex(data) {
 
   uvIn.addClass("badge");
-  // data.current.uvi = 4
 
   if (data.current.uvi < 3) {
     uvIn.removeClass("bg-warning text-dark bg-danger")
@@ -102,4 +106,37 @@ function uvIndex(data) {
     uvIn.removeClass("bg-warning text-dark bg-success");
     uvIn.addClass("bg-danger");
   }
-};
+}
+
+  //5-day forecast
+  function getFiveDayForecast(lat, lon) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherURL}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Five day forecast!!!");
+        console.log(data);
+        var i;
+        for(i = 1; i < 6; i++){
+            console.log("doit " + i*7);
+            console.log(data.list[i*7].dt);
+            console.log(data.list[i*7].dt_txt);
+            var currentDate = data.list[i*7].dt;
+            var date =  new Date(currentDate * 1000);
+            console.log(date);
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+    
+            var weatherIcon = data.list[i*7].weather[0].icon;
+            var iconUrl = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+            console.log(iconUrl);
+            $("#dateE5"+i).text(`Date: ${month}/${day}/${year}`);
+            $("#tempE5"+i).text(`Temp: ${Math.floor(data.list[i*7].main.temp)}°F`);
+            $("#windE5"+i).text(`Wind:  ${Math.floor(data.list[i*7].wind.speed)} MPH`);
+            $("#humidE5"+i).text(`Humidity: ${Math.floor(data.list[i*7].main.humidity)}%`);
+            $("#header-icon5"+i).attr("src", iconUrl);
+          
+          }
+      });
+  }
